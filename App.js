9931bdc01ff4edb0b8419b0254df802f080d5a72
makos.dev <105432079+makosdev@users.noreply.github.com>
2023-04-27 19:38:55 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  const openYoutube = () => {
+    window.open("https://www.youtube.com/", "_blank");
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={openYoutube} className="Youtube-button">
+          Open YouTube
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
